2ab1c9497ce3be80232f587801cb49291b442392
Ritik Ahirwar <ritik.ahirwar020@gmail.com>
2026-04-21 21:48:19 +0530

    Updation of variable

---
+++ Basic/updationOfVariable.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 10;
+    cout<<a<<endl;
+    a = 20;
+    cout<<a<<endl;
+    a = a + 10;
+    cout<<a<<endl;
+    a = a - 4;
+    cout<<a<<endl;;
+
+}
