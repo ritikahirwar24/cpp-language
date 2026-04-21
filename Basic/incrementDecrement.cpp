09262401b68b21e190c70eddc868812e1d72dd53
Ritik Ahirwar <ritik.ahirwar020@gmail.com>
2026-04-21 21:51:06 +0530

    Increment and decrement

---
+++ Basic/incrementDecrement.cpp
@@ -0,0 +1,32 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x = 5;
+//     cout<<x<<endl;
+//     cout<<x++<<endl; // post increment
+//     cout<<x<<endl;
+// }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x = 5;
+//     cout<<x<<endl;
+//     cout<<++x<<endl; // pre increment
+//     cout<<x<<endl;
+// }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x = 5;
+//     cout<<x<<endl;
+//     cout<<x--<<endl; // post decrement
+//     cout<<x<<endl;
+// }
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 5;
+    cout<<x<<endl;
+    cout<<--x<<endl; // pre decrement
+    cout<<x<<endl;
+}
