57208ddb638ce40dfd3ab0f105635083b9ba9461
Ritik Ahirwar <ritik.ahirwar020@gmail.com>
2026-05-04 23:16:42 +0530

    Add program to display multiplication table of a user-defined number

---
+++ Loop/tableOf19.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    for(int i=1; i<=10; i++){
+        cout<<n<<" x "<<i<<" = "<<n*i<<endl;
+    }
+        // for(int i=19; i<=190; i+=19){
+        // cout<<i<<" ";
+        // }
+}
