3014f5ce9b451b606268ac7b26b9521980826bdf
Ritik Ahirwar <ritik.ahirwar020@gmail.com>
2026-05-20 22:42:47 +0530

    Add program to reverse a given number

---
+++ Loop/ReverseTheNo.cpp
@@ -0,0 +1,16 @@
+//Write a progrsm to reverse the given number.
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    int r=0;
+    while(n!=0){
+       int ld = n%10;
+       r*=10;
+       r+=ld;
+       n/=10;
+    }
+    cout<<"Reverse of the number is: "<<r;
+}
