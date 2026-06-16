b5cc45fc6b3939ec882d1f87d59a7ad71b41881a
Ritik Ahirwar <ritik.ahirwar020@gmail.com>
2026-06-16 10:24:26 +0530

    Add program to print a number square pattern

---
+++ Pattern/NumberSquare.cpp
@@ -0,0 +1,16 @@
+//WAP to print:1234 1234 1234 1234
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter number of sides: ";
+    cin>>n;
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+
+}
